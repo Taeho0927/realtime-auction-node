5d4b4d38b5968caf5d96b1519cf95078ea651431
Taeho0927 <rlfxogh0927@gmail.com>
2023-03-27 20:20:51 +0900

    renderMain bugfix & socket.io, SSE 연동

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ const indexRouter = require('./routes/index');
 const authRouter = require('./routes/auth');
 const { sequelize } = require('./models');
 const passportConfig = require('./passport')
+const sse = require('./sse');
+const webSocket = require('./socket');
 
 const app = express();
 passportConfig();
@@ -58,12 +60,15 @@ app.use((req, res, next)=>{
 });
 
 app.use((err, req, res, next)=>{
-    res.locals.message = err.messagel
+    res.locals.message = err.message;
     res.locals.error = process.env.NODE_ENV !== 'production'? err :{};
     res.status(err.status || 500);
     res.render('error'); 
 });
 
-app.listen(app.get('port'), ()=>{
+const server = app.listen(app.get('port'), ()=>{
     console.log(app.get('port'),'번에서 대기중');
 });
+
+webSocket(server, app);
+sse(server);
